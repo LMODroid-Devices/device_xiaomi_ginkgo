4d3bebf6e28ba9e05fe94c52afe7a59133af9a16
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:53:41 +0000

    Adapt to LMODroid by device-add script

--- device.mk
+++ device.mk
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lmodroid
 
 PRODUCT_PACKAGES += \
     ApertureGinkgo \
